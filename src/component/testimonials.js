087eb4c8a533ee4122ccc3d5711e96a0b719dfd3
risa <risairma21@gmail.com>
2023-05-14 21:55:05 +0700

    refactor: add responsive to testimonials section

--- src/component/testimonials.js
+++ src/component/testimonials.js
@@ -5,22 +5,39 @@ import 'owl.carousel/dist/assets/owl.theme.default.css';
 import testiMan from "./../assets/testimonial/man.svg";
 import testiWomen from "./../assets/testimonial/women.svg";
 
+const options = {
+    center: true,
+    items:3,
+    nav: true,
+    merge : true,
+    loop : true,
+    dots :false,
+    margin:30,
+    responsive: {
+      0: {
+          items: 1
+      },
+      600: {
+          items: 1
+      },
+      1000: {
+          items: 2
+      }
+  }
+
+}
+
 
 const testimonials = ()=>{
    return(
     <OwlCarousel 
-    className="owl-theme"
-    center
-    items={2}
-    nav
-    merge
-    loop
-    dots ={false}
-    margin={30}>
+    className=" owl-theme"
+    {...options}
+    >
 
         <div className="item d-flex bg__grey py-3" id='testimonial' >
-              <img src={testiMan} alt="testi 1" className="mx-5"/>
-              <div className="testi__info py-4 me-2">
+              <img src={testiMan} alt="testi 1" className="mx-4"/>
+              <div className="testi__info py-4 me-4">
                 <div class="testi__rate d-flex mb-2">
                       <a href="/#"><i class="bi bi-star-fill text-warning"></i></a>
                       <a href="/#"><i class="bi bi-star-fill text-warning"></i></a>
@@ -32,8 +49,8 @@ const testimonials = ()=>{
               </div>
         </div>
         <div className="item d-flex bg__grey py-3" id='testimonial' >
-              <img src={testiWomen} alt="testi 1" className="mx-5"/>
-              <div className="testi__info py-4 me-2">
+              <img src={testiWomen} alt="testi 1" className="mx-4"/>
+              <div className="testi__info py-4 me-4">
                 <div class="testi__rate d-flex mb-2">
                       <a href="/#"><i class="bi bi-star-fill text-warning"></i></a>
                       <a href="/#"><i class="bi bi-star-fill text-warning"></i></a>
@@ -45,8 +62,8 @@ const testimonials = ()=>{
               </div>
         </div>
         <div className="item d-flex bg__grey py-3" id='testimonial'  >
-              <img src={testiMan} alt="testi 1" className="mx-5"/>
-              <div className="testi__info py-4 me-2">
+              <img src={testiMan} alt="testi 1" className="mx-4"/>
+              <div className="testi__info py-4 me-4">
                 <div class="testi__rate d-flex mb-2">
                       <a href="/#"><i class="bi bi-star-fill text-warning"></i></a>
                       <a href="/#"><i class="bi bi-star-fill text-warning"></i></a>
@@ -58,8 +75,8 @@ const testimonials = ()=>{
               </div>
         </div>
         <div className="item d-flex bg__grey py-3" id='testimonial'  >
-              <img src={testiWomen} alt="testi 1" className="mx-5"/>
-              <div className="testi__info py-4 me-2">
+              <img src={testiWomen} alt="testi 1" className="mx-4"/>
+              <div className="testi__info py-4 me-4">
                 <div class="testi__rate d-flex mb-2">
                       <a href="/#"><i class="bi bi-star-fill text-warning"></i></a>
                       <a href="/#"><i class="bi bi-star-fill text-warning"></i></a>
