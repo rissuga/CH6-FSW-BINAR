f08c76a134fb02d5b06496267390fe5a11db9a5d
risa <risairma21@gmail.com>
2023-05-14 19:54:20 +0700

    refactor: rename className on banner section

--- src/component/banner.js
+++ src/component/banner.js
@@ -2,12 +2,12 @@ const banner = ()=>{
     return (
         
     <section id="banner" class="py-5 mb-2" >
-        <div class="container py-5 banner-section px-5">
-            <h1 class="font__heading__1">Sewa Mobil di (Lokasimu) Sekarang</h1>
-            <p class="mb-5">
+        <div className="container py-5 banner-section px-5">
+            <h1 className="font__heading__1">Sewa Mobil di (Lokasimu) Sekarang</h1>
+            <p className="mb-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             </p>
-            <a class="btn btn-success banner-btn mb-3">
+            <a className="btn btn-success banner-btn mb-3" href="/#">
                 Mulai Sewa Mobil
             </a>
         </div>
